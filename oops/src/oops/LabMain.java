61894127b6822a300a40b3db4958a0560f4c99ca
Aakash <aakashmaneak@gmail.com>
2024-11-10 18:11:35 +0530

    Adding oops

---
+++ oops/src/oops/LabMain.java
@@ -0,0 +1,29 @@
+package oops;
+
+public class LabMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("Hello World");
+		
+//		String s1 = new String("walking");
+//		String s2 = "running";
+//		String s3 = "running";
+//		System.out.println(s2==s3);//true
+//		
+//		String str = "india";
+//		String str2 = new String(str);
+//		System.out.println(str==str2);//false
+//		
+		
+		String str1 = "java";
+		String str2 = "python";
+		int out1 = str1.compareTo(str2);
+		int out2 = str2.compareTo(str1);
+		System.out.println(out1);
+		System.out.println(out2);
+		
+		
+	}
+
+}
